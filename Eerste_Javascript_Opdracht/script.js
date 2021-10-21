2efb6d6b7fec31392e316cde528af9e1c78f57c6
Bierloga <90850242+Bierloga@users.noreply.github.com>
2021-10-21 21:33:13 +0200

    Add files via upload

---
+++ Eerste_Javascript_Opdracht/script.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy");
+    let firstName = 'Lucas';
+    console.log(firstName);
+    //variabele maken met if kan niet omdat het een gereserveerde term is
+    console.log(4+4);
+    console.log("4"+"4");
+    console.log(5-3);
+    console.log(4*3);
+    console.log(10/4);
+    console.log(3%12);
+    let age = 33;
+    console.log(typeof age);
+    age = "drieendertig";
+    console.log(typeof age);
